3aea0f02da648492b49698ed581ecb3b3db32d90
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2024-02-24 02:06:42 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+locals {
+  license                               = "LicenseIncluded"
+  tier                                  = "GeneralPurpose"
+  sku_name                              = "GP_Gen5"
+  family                                = "Gen5"
+}
+data "azurerm_mssql_server" "dct_mssql_server" {
+  name                = var.name
+  resource_group_name = var.sql-rg
+}
+resource "azurerm_mssql_elasticpool" "sql-elastic-pool" {
+  name                = "${var.name}-elastic-pool"
+  resource_group_name = data.azurerm_mssql_server.dct_mssql_server.resource_group_name
+  location            = data.azurerm_mssql_server.dct_mssql_server.location
+  server_name         = data.azurerm_mssql_server.dct_mssql_server.name
+  license_type        = local.license
+  max_size_gb         = var.size
+
+  sku {
+    name     = local.sku_name
+    tier     = local.tier
+    capacity = var.capacity
+    family   = local.family
+  }
+
+  per_database_settings {
+    min_capacity = var.min_capacity
+    max_capacity = var.max_capacity
+  }
+}
